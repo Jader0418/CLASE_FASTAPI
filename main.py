73ccb1dda2361a4d40b9b843b7e9402b6f7ce4bc
Jader Santiago Nieves <jsnieves39@ucatolica.edu.co>
2026-03-27 17:51:14 -0500

    FASTAPI

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
-@app.get("/")
 
+productos = {
+    1: {"nombre": "Teclado", "stock": 10, "valor": 50000},
+    2: {"nombre": "Mouse", "stock": 5, "valor": 30000},
+    3: {"nombre": "Monitor", "stock": 2, "valor": 800000}
+}
+
+@app.get("/")
 async def incio():
     return {"message": "Hola, ya estoy usando FastAPI! "}
 
@@ -11,7 +17,17 @@ async def saludo(nombre: str):
     return {"message": f"Hola {nombre}, bienvenido a FastAPI!"}
 
 @app.get("/cuadrado/{numero}")
-async def cuadrado(numero: int):
-    cuadrado = numero ** numero
+async def calcular_cuadrado(numero: int):
+    cuadrado = numero * numero
     return {"numero": numero, "cuadrado": cuadrado}
 
+
+@app.get("/producto/{id}")
+async def obtener_producto(id: int):
+    
+    return {
+        "id": id,
+        "nombre": productos[id]["nombre"],
+        "stock": productos[id]["stock"],
+        "valor": productos[id]["valor"]
+    }
