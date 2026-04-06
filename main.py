a9cb6fc9e5c3713338fd7c5a65bb68627d511f88
Jader Santiago Nieves <jsnieves39@ucatolica.edu.co>
2026-04-06 17:20:06 -0500

    AGREGAR LOS METODOS FALTANTES ELIMINAR Y ACTUALIZAR

--- main.py
+++ main.py
@@ -37,3 +37,47 @@ async def obtener_producto(id: int):
         "stock": productos[id]["stock"],
         "valor": productos[id]["valor"]
     }
+    
+@app.post("/producto/{id}")
+async def crear_producto(id: int, nombre: str, stock: int, valor: float):
+    if id in productos:
+        raise HTTPException(status_code=400, detail="El producto ya existe")
+
+    productos[id] = {
+        "nombre": nombre,
+        "stock": stock,
+        "valor": valor
+    }
+
+    return {"mensaje": "Producto creado", "producto": productos[id]}
+
+
+# Actualizar producto
+@app.put("/producto/{id}")
+async def actualizar_producto(id: int, nombre: str, stock: int, valor: float):
+    if id not in productos:
+        raise HTTPException(status_code=404, detail="Producto no encontrado")
+
+    productos[id] = {
+        "nombre": nombre,
+        "stock": stock,
+        "valor": valor
+    }
+
+    return {"mensaje": "Producto actualizado", "producto": productos[id]}
+
+
+# Eliminar producto 
+@app.delete("/producto/{id}")
+async def eliminar_producto(id: int):
+    if id not in productos:
+        
+        raise HTTPException(status_code=404, detail="Producto no encontrado") 
+# raise : generar un error / detiene inmediamente la ejecución de la función y devuelve una respuesta con el código de estado y el mensaje especificados.
+# HTPException(...)  Es el tipo de error que estás enviando / No es un error cualquiera / Es un error especial para APIs web
+# 404: significa que el recurso solicitado no se encontró en el servidor / El cliente hizo una solicitud para un recurso que no existe
+
+
+    producto_eliminado = productos.pop(id)
+
+    return {"mensaje": "Producto eliminado", "producto": producto_eliminado}
